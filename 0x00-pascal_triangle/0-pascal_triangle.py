a46c6f5291c96fb804a66bc02796f2a9a9864f09
Chima Gideon Nweke <nweke_chima@yahoo.com>
2024-10-02 12:45:34 +0000

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''A module for working with Pascal's triangle.
+'''
+
+
+def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    if type(n) is not int or n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for i in range(1, len(prev_row)):
+            new_row.append(prev_row[i-1] + prev_row[i])
+
+        new_row.append(1)
+        triangle.append(new_row)
+    
+
+    return triangle
